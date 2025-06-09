6754c2658f05a13001a6f1a77f0d8e35a731bc90
Zain-ul-abdeen-773 <167418111+Zain-ul-abdeen-773@users.noreply.github.com>
2025-06-09 23:44:23 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,14 @@
+from RPS_game import play, quincy, abbey, kris, mrugesh
+from RPS import player
+
+print("\nPlaying against Quincy:")
+play(player, quincy, 1000)
+
+print("\nPlaying against Abbey:")
+play(player, abbey, 1000)
+
+print("\nPlaying against Kris:")
+play(player, kris, 1000)
+
+print("\nPlaying against Mrugesh:")
+play(player, mrugesh, 1000)
